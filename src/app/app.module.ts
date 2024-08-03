a0b30f629197f2890e7657495db407cd7bce6fa1
Rodrigo Linares <rodolinares19@gmail.com>
2024-08-02 20:05:09 -0500

    chore: Add prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common'
+import { provideHttpClient } from '@angular/common/http'
+import en from '@angular/common/locales/en'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { NZ_I18N } from 'ng-zorro-antd/i18n'
+import { en_US } from 'ng-zorro-antd/i18n'
 
-registerLocaleData(en);
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+
+registerLocaleData(en)
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [
     { provide: NZ_I18N, useValue: en_US },
     provideAnimationsAsync(),
@@ -29,4 +24,4 @@ registerLocaleData(en);
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
