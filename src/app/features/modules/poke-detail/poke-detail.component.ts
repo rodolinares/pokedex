05c1c5b51b01f1b54e506ce7caac951adeb3782e
Rodrigo Linares <rodolinares19@gmail.com>
2024-08-03 21:16:09 -0500

    Refactor poke-detail component to improve navigation and add pagination

--- src/app/features/modules/poke-detail/poke-detail.component.ts
+++ src/app/features/modules/poke-detail/poke-detail.component.ts
@@ -12,9 +12,12 @@ import { PokemonService } from '@features/services/pokemon.service'
   styleUrl: './poke-detail.component.css'
 })
 export class PokeDetailComponent implements OnInit {
+  canGoAhead = false
+  canGoBack = false
   error = false
   loading = true
   pokemon?: PokemonDetail
+  total = 10277
 
   constructor(
     private route: ActivatedRoute,
@@ -22,26 +25,41 @@ export class PokeDetailComponent implements OnInit {
     private pokemonService: PokemonService
   ) {}
 
-  async ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id')
-    if (!id) return
+  ngOnInit() {
+    this.route.params.subscribe(async params => {
+      const id = params['id']
 
-    try {
-      this.pokemon = await lastValueFrom(this.pokemonService.get(id))
-      this.playCry(this.pokemon.cries.latest)
-      this.loading = false
-    } catch (error) {
-      this.error = true
-      this.loading = false
-    }
+      try {
+        this.pokemon = await lastValueFrom(this.pokemonService.get(id))
+        this.canGoBack = this.pokemon.id > 1
+        this.canGoAhead = this.pokemon.id < this.total
+        this.playCry(this.pokemon)
+        this.loading = false
+      } catch (error) {
+        this.error = true
+        this.loading = false
+      }
+    })
   }
 
   goBack() {
     this.router.navigate(['/pokemon'])
   }
 
-  playCry(source: string) {
-    const audio = new Audio(source)
+  prev() {
+    if (this.pokemon && this.pokemon.id > 1) {
+      this.router.navigate(['/pokemon', this.pokemon.id - 1])
+    }
+  }
+
+  next() {
+    if (this.pokemon && this.pokemon.id < this.total) {
+      this.router.navigate(['/pokemon', this.pokemon.id + 1])
+    }
+  }
+
+  playCry(pokemon: PokemonDetail) {
+    const audio = new Audio(pokemon.cries.legacy)
     audio.volume = 0.1
     audio.play()
   }
--- src/app/features/modules/poke-detail/poke-detail.module.ts
+++ src/app/features/modules/poke-detail/poke-detail.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
 import { ArrowLeftOutline } from '@ant-design/icons-angular/icons'
+import { NzButtonModule } from 'ng-zorro-antd/button'
 import { NzGridModule } from 'ng-zorro-antd/grid'
 import { NzIconModule } from 'ng-zorro-antd/icon'
 import { NzListModule } from 'ng-zorro-antd/list'
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header'
+import { NzSpaceModule } from 'ng-zorro-antd/space'
 import { NzTagModule } from 'ng-zorro-antd/tag'
 
 import { SpinnerComponent } from '@shared/components/spinner/spinner.component'
@@ -19,10 +21,12 @@ const icons = [ArrowLeftOutline]
   declarations: [PokeDetailComponent],
   imports: [
     CommonModule,
+    NzButtonModule,
     NzGridModule,
     NzIconModule.forChild(icons),
     NzListModule,
     NzPageHeaderModule,
+    NzSpaceModule,
     NzTagModule,
     SpinnerComponent,
     PokeDetailRoutingModule
